0f5b754e456d9aac3edc9e1b0163668013708d38
Ricardo Nogueira <seuze.rick@gmail.com>
2019-12-01 18:46:23 -0300

    :sparkles: adds another module with an alias

--- module_playground.exs
+++ module_playground.exs
@@ -2,6 +2,8 @@ defmodule ModulePlayground do
     import IO, only: [puts: 1]
     import Kernel, except: [inspect: 1]
 
+    alias ModulePlayground.Misc.Util.Math, as: MyMath
+
     def say_hello do
         inspect "Hello World!"
     end
@@ -10,5 +12,9 @@ defmodule ModulePlayground do
         puts "Starting output"
         puts param1
         puts "Ending output"
+    end
+
+    def print_sum do
+        MyMath.add(1, 2)
     end
 end
