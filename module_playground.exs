e57f24aa8daf1411c11b819a103a813d94d3a3c6
Ricardo Nogueira <seuze.rick@gmail.com>
2019-12-01 18:31:43 -0300

    :art: add a import use case

--- module_playground.exs
+++ module_playground.exs
@@ -1,5 +1,14 @@
 defmodule ModulePlayground do
+    import IO, only: [puts: 1]
+    import Kernel, except: [inspect: 1]
+
     def say_hello do
-        IO.puts "Hello World!"
+        inspect "Hello World!"
+    end
+
+    def inspect(param1) do
+        puts "Starting output"
+        puts param1
+        puts "Ending output"
     end
 end
